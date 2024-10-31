017942095e7be0b51a3bbbc5c1d3dd68fd1709ea
LestevSA <LestevSA@MGT01-0043>
2024-10-31 15:15:41 +0300

    https://github.com/serge-l1/PMSMCheck.git

--- tests/sport_zones_api_test.py
+++ tests/sport_zones_api_test.py
@@ -1,34 +1,47 @@
-from playwright.sync_api import  APIRequestContext
+from playwright.sync_api import APIRequestContext
 
 def test_should_sports_zone_preview_displays(api_request_context: APIRequestContext) -> None:
-    facilities_response = api_request_context.get("/api/facilities?count=5000&page=1")
+    # Получаем список спортивных объектов
+    facilities_response = api_request_context.get("/api/facilities?count=4770&page=1")
     assert facilities_response.ok
 
     sport_facilities = list(facilities_response.json()["items"])
 
     errors = []
-    
+
     for sport_facility in sport_facilities:
-        sport_zones_response = api_request_context.get(f"/api/facilities/{sport_facility['id']}/sportzones")
+        facility_id = sport_facility.get('id')  # Получаем id спортобъекта
+        
+        # Запрашиваем спортивные зоны для каждого спортивного объекта
+        sport_zones_response = api_request_context.get(f"/api/facilities/{facility_id}/sportzones")
         assert sport_zones_response.ok
 
         sport_zones = list(sport_zones_response.json()["item"]['sportZones'])
-       
+
         for sport_zone in sport_zones:
             try:
-                photo_url = sport_zone['mainPhoto']['url']
-            except:
-                print(f"Failed to open photo for sports zones {sport_facility['id']}")
-                errors.append(sport_facility['id'])
+                # Попытка получить URL изображения превью
+                thumbnail_url = sport_zone['mainPhoto']['thumbnailUrl']
+                thumbnail_url_response = api_request_context.get(thumbnail_url)
+                assert thumbnail_url_response.ok
+            except Exception:
+                print(f"Failed to open preview for sports zone. facilityId: {facility_id}, externalId: {sport_facility.get('externalId')}, name: {sport_facility.get('name')}")
+                errors.append(f"facilityId: {facility_id}, externalId: {sport_facility.get('externalId')} - {sport_facility.get('name')}")
                 continue
-            photo_url_response = api_request_context.get(photo_url)
-            assert photo_url_response.ok
-           
-            if(photo_url_response.body() == b''):
-                print(f"Downloaded file is empty for sports zones {sport_facility['id']} img link: {photo_url}")
-                errors.append(sport_facility['id'])
+
+            # Попытка получить основной URL изображения
+            try:
+                photo_url = sport_zone['mainPhoto']['url']
+                photo_url_response = api_request_context.get(photo_url)
+                assert photo_url_response.ok
+
+                if photo_url_response.body() == b'':
+                    print(f"Downloaded file is empty for sports zone. facilityId: {facility_id}, externalId: {sport_facility.get('externalId')}, name: {sport_facility.get('name')} img link: {photo_url}")
+                    errors.append(f"facilityId: {facility_id}, externalId: {sport_facility.get('externalId')} - {sport_facility.get('name')}")
+
+            except Exception:
+                print(f"Failed to open photo for sports zone. facilityId: {facility_id}, externalId: {sport_facility.get('externalId')}, name: {sport_facility.get('name')}")
+                errors.append(f"facilityId: {facility_id}, externalId: {sport_facility.get('externalId')} - {sport_facility.get('name')}")
+                continue
 
     assert errors == []
-
-  
-
